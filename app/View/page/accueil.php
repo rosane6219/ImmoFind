821445ba50ad531979d43d403d16a11ec4b3b108
kilianDahlem <dahlem.kilian@gmail.com>
2021-12-30 16:06:13 +0100

    banderolle pour la page d'accueil

--- app/View/page/accueil.php
+++ app/View/page/accueil.php
@@ -1,4 +1,23 @@
-<div>
-    <h1>page acceuil</h1>
-</div>
-<p> la phrase acceuil </p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/banderolle.css">
+</head>
+<body>
+    <div id="banderolle">
+        <div class="images"> <!-- ici il faut choisir les images souhaitées -->
+            <img src="" alt=""> 
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </div>
+
+    <p> la phrase acceuil </p>    
+
+</body>
+</html>
