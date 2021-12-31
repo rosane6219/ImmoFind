452982ec50e850f3ca411555373fb03547e8c30c
kilianDahlem <dahlem.kilian@gmail.com>
2021-12-31 14:08:55 +0100

    changement appel css

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -27,7 +27,7 @@ class Controller{
         require($view);
         $content_for_layout = ob_get_clean();
         ob_start();
-        require ROOT.DS.'View'.DS.'css'.DS.'banderole.css';
+        require ROOT.DS.'View'.DS.'css'.DS.'main.css';
         $style_for_content = ob_get_clean();
         require ROOT.DS.'View'.DS.'layout'.DS.$this->layout.'.php';
         $this->rendered = true;
