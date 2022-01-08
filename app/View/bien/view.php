558af067ebda221a1b6ee0e27fe3ae7012f92f9e
rosane6219 <im_benkhelil@esi.dz>
2022-01-08 20:41:18 +0100

    Commit ds modifs

--- app/View/bien/view.php
+++ app/View/bien/view.php
@@ -4,15 +4,15 @@
     <h1>Details du Bien</h1>
 
     <div>
-        <h3> <?php echo $page->titre ?> </h3>
+        <h3> Titre : <?php echo $page->titre ?> </h3>
     </div>
-    <p> <?php echo $page->typeannonce;  ?> <br>
-        <?php echo $page->typebien;  ?> <br>
-        <?php echo $page->prix; ?> <br>
-        <?php echo $page->descrption; ?> <br>
-        <?php echo $page->ville; ?> <br>
-        <?php echo $page->codepostal; ?> <br>
-        <?php echo $page->modif; ?> <br>
+    <p> Type d'annonce : <?php echo $page->typeannonce;  ?> <br>
+        Type du bien : <?php echo $page->typebien;  ?> <br>
+        Prix du bien : <?php echo $page->prix; ?> <br>
+        Ville : <?php echo $page->ville; ?> <br>
+        Code postal :<?php echo $page->codepostal; ?> <br>
+        Description du bien : <?php echo $page->descrption; ?> <br>
+        Emis le : <?php echo $page->modif; ?> <br>
     </p>
     <!--<a href="<?php
                 if (isset($_SESSION['User'])) echo Router::url("panier/add/userid:{$_SESSION['User']->id}/bienid:{$page->id}");
@@ -28,10 +28,10 @@
             document.getElementById("btnImprime").style.display = "block";
         }
     </script>
-    
-    <a id="btnImprime" onclick="printPdf(<?php $_SESSION['PATH_INFO']?>)"> Impression </a> 
 
+    <!--a id="btnImprime" onclick="printPdf(<?php $_SERVER['PATH_INFO']?>)"> Impression </a--> 
+        <button id="btnImprime" onclick="printPdf(<?php $_SERVER['PATH_INFO']?>)"> Imprimer </button> 
     <!-- script de la fonction pour impression -->
     
-    <!--?php  debug($page);?-->
+   
 </div>
